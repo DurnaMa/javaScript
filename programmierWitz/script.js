52f5f99d862665e506b047ffb3437f5c3510febf
DurnaMa <durna.m@outlook.de>
2024-02-17 17:38:49 +0100

    add ein weitern witze

--- programmierWitz/script.js
+++ programmierWitz/script.js
@@ -1,4 +1,9 @@
-function witz1() {
-    document.getElementById("witz").innerHTML =
+function joke1() {
+    document.getElementById("joke").innerHTML =
         "Die Selbsthilfegruppe „HTML-Sonderzeichen-Probleme“ trifft sich heute im gro&szlig ;en Saal.";
+}
+
+function joke2() {
+    document.getElementById("joke").innerHTML =
+        "Theorie ist wenn du alles weißt aber nichts funktioniert. Praxis ist wenn alles funktioniert aber nieman weiß warum. Bei Programmieren ist Praxis und Theorie vereint. Nichts funktioniert und niemand weiß warum.";
 }
