e34de4f216afe919b703f99fa2ce44d4ac5dc67f
DurnaMa <durna.m@outlook.de>
2024-02-17 17:32:23 +0100

    2 button und 2 witze

--- programmierWitz/script.js
+++ programmierWitz/script.js
@@ -1,4 +1,4 @@
-function newJocke() {
+function witz1() {
     document.getElementById("witz").innerHTML =
         "Die Selbsthilfegruppe „HTML-Sonderzeichen-Probleme“ trifft sich heute im gro&szlig ;en Saal.";
 }
