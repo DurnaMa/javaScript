c107bc972e48829a71eb546622ac9ac9ce1067c7
DurnaMa <durna.m@outlook.de>
2024-02-19 10:26:44 +0100

    add funktion parameter angepast

--- funktionsparameter/script.js
+++ funktionsparameter/script.js
@@ -1,5 +1,16 @@
 // Die Funktion greet wir auf gerufen, zeitgleich wird die variavle den "x" zugewisen und "x" wider durch den Namen ersetzt "${x}"
 
-function greet (x) {
-    document.getElementById('greeting').innerHTML = `Hallo ${x} wie geht es dir`;
+// function greet(x) {
+//     document.getElementById(
+//         "greeting"
+//     ).innerHTML = `Hallo ${x} wie geht es dir`;
+// }
+
+function greetFromTextField() {
+    let name = document.getElementById('nameField').value;
+    greet(name);
+}
+
+function greet(x) {
+    document.getElementById('greeting').innerHTML = `Hallo ${x}, wie geht es dir?`
 }
