fc41385d251c5fdf39f7cd5c74770f29dc607169
DurnaMa <durna.m@outlook.de>
2024-02-19 22:35:08 +0100

    add CodingChallenge 3 fertig

--- jsFunction/script.js
+++ jsFunction/script.js
@@ -3,3 +3,14 @@ function runCode() {
     //Bitte hier Code eingeben
     document.getElementById("coolerButton").innerHTML = "Klick mich!";
 }
+
+// Coding-Challenge2
+// Bitte hier Code eingeben
+function favoriteAnimal() {
+    document.getElementById("animalButton").innerHTML = "Giraffe";
+}
+
+function getInput() {
+    let animal = document.getElementById('animal').value; 
+    let myTextContainer = document.getElementById('myText');
+  }
